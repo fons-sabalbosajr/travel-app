ec1871cf5a4cbca6340e87109ad39ae11f18bccd
Fons Sabalbosa <fonssabalbosa.27@gmail.com>
2023-07-12 20:53:29 +0800

    Initialized footer feature

--- gala-app/src/Components/Footer/Footer.jsx
+++ gala-app/src/Components/Footer/Footer.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import video2 from "../../Assets/bgvideo2.mp4";
+import { FiSend } from "react-icons/fi";
+import { SiYourtraveldottv } from "react-icons/si";
+import { FiChevronRight } from "react-icons/fi";
+import {
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted typeof="video/mp4"></video>
+      </div>
 
-export default Footer
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter email address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+              <SiYourtraveldottv className="icon" /> Gala.
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nulla,
+              commodi tenetur sint provident facere et dolore facilis, quis
+              dolores, nemo ducimus! Blanditiis provident at odit laudantium
+              distinctio. Soluta, fuga.
+            </div>
+
+            <div className="footerSocials flex">
+              <AiFillFacebook className="icon" />
+              <AiFillInstagram className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiOutlineTwitter className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            {/*GROUP ONE*/}
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" /> Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" /> Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" /> Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" /> Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" /> Payment
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> 
+  );
+};
+
+export default Footer;
--- gala-app/src/Components/Main/Main.jsx
+++ gala-app/src/Components/Main/Main.jsx
@@ -10,7 +10,6 @@ import img6 from "../../Assets/img6.jpg";
 import img7 from "../../Assets/img7.jpg";
 import img8 from "../../Assets/img8.jpg";
 import img9 from "../../Assets/img9.jpg";
-
 import {
   HiOutlineLocationMarker,
   HiOutlineClipboardCheck,
@@ -23,18 +22,17 @@ const Data = [
     destTitle: "Boracay",
     location: "Philippines",
     grade: "BEACH RELAX ",
-    fees: "$1000",
+    fees: "₱8000",
     description:
       "Welcome to Boracay, a tropical paradise nestled in the heart of the Philippines. Known for its pristine white sand beaches, crystal-clear turquoise waters, and vibrant nightlife, Boracay has become a world-renowned destination for travelers seeking sun, relaxation, and adventure.",
   },
-
   {
     id: 2,
     imgSrc: img2,
     destTitle: "Bohol",
     location: "Philippines",
     grade: "HILLS OF HAPPINESS ",
-    fees: "$3000",
+    fees: "₱3000",
     description:
       "One of the main attractions in Bohol is the world-renowned Chocolate Hills, a geological formation consisting of over a thousand cone-shaped hills that turn brown during the dry season, resembling chocolate mounds. These iconic hills provide an awe-inspiring backdrop for photos and offer a magnificent panoramic view from the viewing decks.",
   },
@@ -44,7 +42,7 @@ const Data = [
     destTitle: "North Windmills",
     location: "Philippines",
     grade: "FRESH TROPICAL AIR ",
-    fees: "$3000",
+    fees: "₱3000",
     description:
       "Located in the northern region of the Philippines, Ilocos is known for its stunning landscapes and rich cultural heritage. One of the captivating features of this region is the iconic windmills that dot its picturesque countryside. These majestic structures, standing tall against the sky, harness the power of the wind and have become a symbol of Ilocos commitment to renewable energy.",
   },
@@ -54,7 +52,7 @@ const Data = [
     destTitle: "Puerto Prinsesa",
     location: "Philippines",
     grade: "TROPICAL CAVES ",
-    fees: "$4000",
+    fees: "₱4000",
     description:
       "Famous for its crystal-clear turquoise waters, white sandy beaches, and vibrant coral reefs, Palawan is a haven for beach lovers, snorkelers, and divers alike. The province boasts stunning destinations like El Nido, Coron, and Puerto Princesa, each offering unique experiences and awe-inspiring landscapes.",
   },
@@ -64,7 +62,7 @@ const Data = [
     destTitle: "Luneta",
     location: "Philippines",
     grade: "HISTORICAL CAPTURE ",
-    fees: "$2000",
+    fees: "₱2000",
     description:
       "Named after Dr. Jose Rizal, the Philippine national hero, Luneta serves as a tribute to his life and the sacrifices he made for the country s independence. The park spans over 140 acres and offers a peaceful and scenic environment amidst the bustling city. It is a place where locals and tourists alike come to relax, enjoy nature, and immerse themselves in the country vibrant history.",
   },
@@ -74,7 +72,7 @@ const Data = [
     destTitle: "Pampanga",
     location: "Philippines",
     grade: "GIANT LANTERN ",
-    fees: "$1500",
+    fees: "₱1500",
     description:
       "Aside from its gastronomic delights, Pampanga offers several attractions for visitors. The annual Hot Air Balloon Festival in Clark Freeport Zone attracts both locals and tourists, showcasing colorful hot air balloons against the backdrop of the Clark Airfield. The province is also home to numerous cultural festivals, such as the renowned Giant Lantern Festival in the city of San Fernando, where massive and intricately designed lanterns illuminate the night sky.",
   },
@@ -84,7 +82,7 @@ const Data = [
     destTitle: "Batanes",
     location: "Philippines",
     grade: "OPEN FIELDS DREAM ",
-    fees: "$5500",
+    fees: "₱5500",
     description:
       'Batanes is often referred to as the "Home of the Winds" due to its gusty weather, but don t let that deter you. This rugged charm adds to the allure of the destination, providing a unique experience for travelers seeking tranquility and natural wonders.',
   },
@@ -94,7 +92,7 @@ const Data = [
     destTitle: "Aurora",
     location: "Philippines",
     grade: "SURFING PLEASURES ",
-    fees: "$6000",
+    fees: "₱6000",
     description:
       'Aurora, Philippines is a captivating province located on the eastern coast of Luzon, the largest island in the Philippines. Known for its breathtaking natural beauty and untouched landscapes, Aurora offers a unique blend of lush rainforests, majestic waterfalls, pristine beaches, and towering mountains. Named after the wife of former President Manuel L. Quezon, Aurora is often referred to as the "Surfing Capital of the Philippines" due to its world-class waves and renowned surfing spots.',
   },
@@ -104,7 +102,7 @@ const Data = [
     destTitle: "Tagaytay",
     location: "Philippines",
     grade: "TAAL VIEWDECK ",
-    fees: "$9000",
+    fees: "₱9000",
     description:
       "Tagaytay is a charming city nestled in the province of Cavite, Philippines. Located just a short distance from the bustling capital of Manila, Tagaytay is renowned for its cool climate, scenic landscapes, and breathtaking views. Perched on the ridge of the majestic Taal Volcano, it offers visitors a mesmerizing panorama of Taal Lake and its picturesque surroundings.",
   },
